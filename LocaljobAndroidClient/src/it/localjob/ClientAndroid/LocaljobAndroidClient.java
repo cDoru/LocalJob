be0908c2c07ec60fe23be020e774599d8d031680
Luca Cavina <luca.cavina@gmail.com>
2013-03-15 14:37:26 +0100

    update mobile version

--- LocaljobAndroidClient/src/it/localjob/ClientAndroid/LocaljobAndroidClient.java
+++ LocaljobAndroidClient/src/it/localjob/ClientAndroid/LocaljobAndroidClient.java
@@ -30,7 +30,8 @@ public class LocaljobAndroidClient extends DroidGap
         super.onCreate(savedInstanceState);
         // Set by <content src="index.html" /> in config.xml
         //super.loadUrl(Config.getStartUrl());
-        super.loadUrl("file:///android_asset/www/index.html");
+        //super.loadUrl("file:///android_asset/www/index.html");
+        super.loadUrl("file:///android_asset/www/prova_index.html");
     }
 }
 
