7646e1521bd04afd9eb0527ae85a72623fa31c4b
Luca Cavina <luca.cavina@gmail.com>
2013-03-19 11:29:16 +0100

    Update Login and Signin

---
+++ LocaljobAndroidClient/assets/www/function.js
@@ -0,0 +1,23 @@
+﻿function login()	//gestisce il login dell'utente
+{
+	alert("prova!");
+	/*
+	serverDaUsare();
+	valoreDigitato =  $('#nickname').val(); 
+	if(valoreDigitato == "") alert("Inserisci il tuo nick");
+	else
+	{
+       $.ajax({
+          type: 'post',
+          url: ''+serverScelto+'/login',
+          contentType: 'application/x-www-form-urlencoded',
+          data: {'username': valoreDigitato},
+          success: ajaxLOGIN,
+          error: errorHandler
+       })
+	}
+	*/
+}
+
+
+
