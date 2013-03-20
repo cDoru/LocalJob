f62e8105ac6c84e6f7b1414bb04a7a369c9fa2a9
Luca Cavina <luca.cavina@gmail.com>
2013-03-20 11:06:05 +0100

    Create user and pro signIn

--- LocaljobAndroidClient/assets/www/function.js
+++ LocaljobAndroidClient/assets/www/function.js
@@ -21,12 +21,61 @@ function login()	//gestisce il login dell'utente
           success: ajaxLOGIN,
           error: errorHandler
        })
+}
+
+//Registrazione utente
+function user_signin()
+{
+	user =  $('#signU_user').val(); 
+	password1 =  $('#signU_password1').val();
+	password2 =  $('#signU_password2').val();
+	mail =  $('signU_mail').val();
+	tipoUtente = "cliente";
 	
+	if(password1 != password2){
+		alert("Le due password sono diverse");
+	}
+	else 
+	//fai il controllo che sia inserita una mail corretta
+	//if
+	//else
+	{
+		$.ajax({
+	          type: 'POST',
+	          url: 'http://95.141.45.174/register',
+	          //contentType:"application/json",
+	          //dataType:'jsonp',
+	          contentType: 'application/x-www-form-urlencoded',
+	          crossDomain: true,
+	          data: {'userId': user, 'password': password, 'email': mail, 'password': password, 'tipoUtente': tipoUtente},
+	          success: ajaxSIGNIN,
+	          error: errorHandler
+	   })
+	}
+}
+
+//Registrazione Professionista
+function pro_signin()
+{
+	user =  $('#signP_user').val(); 
+	password1 =  $('#signP_password1').val();
+	password2 =  $('#signP_password2').val();
+	mail =  $('signP_mail').val();
+	
+	if(password1 != password2){
+		alert("Le due password sono diverse");
+	}
+	else{
+		alert("ok");
+	}
 }
 
 
-
 function ajaxLOGIN(data){
+	alert(data);
+}
+
+function ajaxSIGNIN(data){
 	alert(data);
 }
 
