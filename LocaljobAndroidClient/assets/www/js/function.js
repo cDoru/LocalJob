2addf949f6f8d04af98f50297c94b6bfe7443994
Luca Cavina <luca.cavina@gmail.com>
2013-03-28 00:13:46 +0100

    Update user and pro signin - create top nav bar with icon for notifications

--- LocaljobAndroidClient/assets/www/js/function.js
+++ LocaljobAndroidClient/assets/www/js/function.js
@@ -8,69 +8,19 @@ $(document).ready(function()	//funzioni che si avviano al caricamento della pagi
 	var firstYear = 1920;
 	var lastYear = 1999;
 	for(var i =firstYear; i<=lastYear; i++) {
-	       $('#anni').append('<option>'+i+'</option>')
+	       $('#anni').append('<option>'+i+'</option>');
 	}
 	
-	
-	//roba per la validazione email
-	$('#contact-form').validate(
-	{
-	rules: {
-		 signP_user: {
-			 minlength: 2,
-			 maxlength: 30,
-			 required: true
-		 },
-		 signP_mail: {
-			 required: true,
-			 email: true
-		 },
-		 password: {
-		     minlength: 8,
-		     required: true
-		},
-		signP_password2: {
-			minlength: 8,
-			equalTo: "#password",
-		    required: true
-	    },
-		signU_user: {
-			 minlength: 2,
-			 maxlength: 30,
-			 required: true
-		 },
-		 signU_mail: {
-			 required: true,
-			 email: true
-		 },
-		signU_password2: {
-			minlength: 8,
-			equalTo: "#password",
-		    required: true
-	    },
-		 signU_name: {
-			 required: true
-			 //digits: true
-		 },
-		 signU_surname: {
-			 required: true
-			 //digits: true
-		 },
-		 signU_phone: {
-			 required: false,
-			 number: true
-		 }
-	},
-	highlight: function(element) {
-	    $(element).closest('.control-group').removeClass('success').addClass('error');
-		$(element).closest('.control-group').addClass('error');
-	},
-	success: function(element) {
-	    element
-	    //.text('OK!').addClass('valid')
-	    .closest('.control-group').removeClass('error').addClass('success');
-	}
-	});
+	//roba per il tooltip
+	$('#prova').popover({
+		'html': 'true',
+        'selector': '',
+        'placement': 'bottom',
+        'title': 'Notifiche',
+        'content': '<div class="well well-small">Guarda il tutorial di Local Job</div>'+
+        '<div class="well well-small">Aggiorna il tuo profilo per iniziare</div>',
+        'container': '#prova_prova'
+      });
 });
 
 
@@ -184,6 +134,10 @@ function pro_signin()
         success: ajaxSIGNIN,
         error: errorHandler
 	})
+}
+
+function provaNotifica(){
+	$('#notifica').popover('toggle');
 }
 
 function cambiaBottone(testo){
---
+++ LocaljobAndroidClient/assets/www/js/function_validate.js
@@ -0,0 +1,74 @@
+$(document).ready(function()	//funzioni che si avviano al caricamento della pagina
+{
+	jQuery.support.cors = true;
+	
+	//script per il form della data di nascita
+	var firstYear = 1920;
+	var lastYear = 1999;
+	for(var i =firstYear; i<=lastYear; i++) {
+	       $('#anni').append('<option>'+i+'</option>');
+	}
+	
+	
+	//roba per la validazione email
+	$('#contact-form').validate(
+	{
+	rules: {
+		 signP_user: {
+			 minlength: 2,
+			 maxlength: 30,
+			 required: true
+		 },
+		 signP_mail: {
+			 required: true,
+			 email: true
+		 },
+		 password: {
+		     minlength: 8,
+		     required: true
+		},
+		signP_password2: {
+			minlength: 8,
+			equalTo: "#password",
+		    required: true
+	    },
+		signU_user: {
+			 minlength: 2,
+			 maxlength: 30,
+			 required: true
+		 },
+		 signU_mail: {
+			 required: true,
+			 email: true
+		 },
+		signU_password2: {
+			minlength: 8,
+			equalTo: "#password",
+		    required: true
+	    },
+		 signU_name: {
+			 required: true
+			 //digits: true
+		 },
+		 signU_surname: {
+			 required: true
+			 //digits: true
+		 },
+		 signU_phone: {
+			 required: false,
+			 number: true
+		 }
+	},
+	highlight: function(element) {
+	    $(element).closest('.control-group').removeClass('success').addClass('error');
+		$(element).closest('.control-group').addClass('error');
+	},
+	success: function(element) {
+	    element
+	    //.text('OK!').addClass('valid')
+	    .closest('.control-group').removeClass('error').addClass('success');
+	}
+	});
+	
+});
+
