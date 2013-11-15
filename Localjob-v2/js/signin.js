8ae3c113ec5c5122054f5d4f1cbf44d61c893492
mcinti <mcinti14@gmail.com>
2013-11-15 20:51:56 +0100

    fixing signin_consumer_personal page

--- Localjob-v2/js/signin.js
+++ Localjob-v2/js/signin.js
@@ -1,6 +1,27 @@
 function cliccabile(){
 	$(".role_name").click(function(){
-                 window.location=$(this).find("a").attr("href");
-                 return false;
-                });
+        window.location=$(this).find("a").attr("href");
+        return false;
+    });
+}
+/*function completeMenu(){
+	//completeDay();
+	competeMonth();
+}*/
+/*function completeDay(){
+	$("#dayList").html("<li><a href='#'>blablabla</a></li>");
+}*/
+function completeMonth(){
+	$("#monthList").html("<li><a href='#'>Gen</a></li>");
+	$("#monthList").append("<li><a href='#'>Feb</a></li>");
+	$("#monthList").append("<li><a href='#'>Mar</a></li>");
+	$("#monthList").append("<li><a href='#'>Apr</a></li>");
+	$("#monthList").append("<li><a href='#'>Mag</a></li>");
+	$("#monthList").append("<li><a href='#'>Giu</a></li>");
+	$("#monthList").append("<li><a href='#'>Lug</a></li>");
+	$("#monthList").append("<li><a href='#'>Ago</a></li>");
+	$("#monthList").append("<li><a href='#'>Set</a></li>");
+	$("#monthList").append("<li><a href='#'>Ott</a></li>");
+	$("#monthList").append("<li><a href='#'>Nov</a></li>");
+	$("#monthList").append("<li><a href='#'>Dic</a></li>");
 }
